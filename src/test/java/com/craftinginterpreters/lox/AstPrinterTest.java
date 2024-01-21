849e5bcbb42c82f9fc478d5b470b9304d03480d8
Pranav Bijapur <bijapurpranav@gmail.com>
2024-01-20 19:20:18 -0800

    ch5: add AstPrinter and unit test

---
+++ src/test/java/com/craftinginterpreters/lox/AstPrinterTest.java
@@ -0,0 +1,21 @@
+package com.craftinginterpreters.lox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AstPrinterTest {
+
+    @Test
+    void testAstPrint() {
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.MINUS, "-", null, 1),
+                new Expr.Literal(123)),
+            new Token(TokenType.STAR, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67)));
+
+        assertEquals("(* (- 123) (group 45.67))", new AstPrinter().print(expression));
+    }
+}
